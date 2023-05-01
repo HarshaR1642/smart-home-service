1550fdc29721328ebd830d553334c652dbabab61
HarshaR1642 <sriharsha@rently.com>
2023-05-02 00:05:48 +0530

    launch app on boot complete

--- app/src/main/java/com/service/keylessrn/Constants.java
+++ app/src/main/java/com/service/keylessrn/Constants.java
@@ -6,5 +6,9 @@ public final class Constants {
     public static final String ACTION_ENABLE_LOCK_MODE = "android.intent.action.ENABLE_LOCK_MODE";
     public static final String ACTION_ENABLE_LOCK_MODE_SUCCESS = "android.intent.action.ENABLE_LOCK_MODE_SUCCESS";
     public static final String ACTION_DISABLE_LOCK_MODE = "android.intent.action.DISABLE_LOCK_MODE";
+    public static final String CLIENT_APP_PACKAGE = "rocks.keyless.app.android";
+    public static final String CLIENT_APP_MAIN_CLASS = "com.rently.keylessrn.MainActivity";
+    public static final String CLIENT_APP_RECEIVER_CLASS = "com.rently.keylessrn.Receiver";
+
 
 }
--- app/src/main/java/com/service/keylessrn/Receiver.java
+++ app/src/main/java/com/service/keylessrn/Receiver.java
@@ -13,17 +13,52 @@ public class Receiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         Log.i(Constants.TAG, "Broadcast Received " + action);
-        switch (action) {
-            case Constants.ACTION_ENABLE_LOCK_MODE: {
+        if (action.equals(Constants.ACTION_ENABLE_LOCK_MODE)) {
+            try {
+                DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+                if (dpm.isDeviceOwnerApp(context.getPackageName())) {
+                    ComponentName admin = new ComponentName(context, SmartHomeDeviceAdminReceiver.class);
+                    dpm.setLockTaskPackages(admin, new String[]{Constants.CLIENT_APP_PACKAGE});
+                    Intent successIntent = new Intent(Constants.ACTION_ENABLE_LOCK_MODE_SUCCESS);
+                    successIntent.setComponent(new ComponentName(Constants.CLIENT_APP_PACKAGE, Constants.CLIENT_APP_RECEIVER_CLASS));
+                    context.sendBroadcast(successIntent);
+                } else {
+                    Log.i(Constants.TAG, "App is not an owner");
+                }
+            } catch (Exception e) {
+                Log.e(Constants.TAG, "Error in enableLockMode");
+                e.printStackTrace();
+            }
+        } else if (action.equals(Constants.ACTION_DISABLE_LOCK_MODE)) {
+            try {
+                DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+                if (dpm.isDeviceOwnerApp(context.getPackageName())) {
+                    dpm.clearDeviceOwnerApp(context.getPackageName());
+                    ComponentName admin = new ComponentName(context.getApplicationContext(), SmartHomeDeviceAdminReceiver.class);
+                    dpm.removeActiveAdmin(admin);
+                    Log.i(Constants.TAG, "Removed as device owner");
+                } else {
+                    Log.i(Constants.TAG, "App is not an owner");
+                }
+            } catch (Exception e) {
+                Log.e(Constants.TAG, "Error in disableLockMode");
+                e.printStackTrace();
+            }
+        } else if (action.equals(Intent.ACTION_PACKAGE_ADDED) || action.equals(Intent.ACTION_PACKAGE_REPLACED)) {
+            String packageName = intent.getData().getSchemeSpecificPart();
+            Log.i(Constants.TAG, packageName);
+            if (packageName.equals(Constants.CLIENT_APP_PACKAGE)) {
                 try {
                     DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
 
                     if (dpm.isDeviceOwnerApp(context.getPackageName())) {
                         ComponentName admin = new ComponentName(context, SmartHomeDeviceAdminReceiver.class);
-                        dpm.setLockTaskPackages(admin, new String[]{"rocks.keyless.app.android"});
-                        Intent successIntent = new Intent(Constants.ACTION_ENABLE_LOCK_MODE_SUCCESS);
-                        successIntent.setComponent(new ComponentName("rocks.keyless.app.android", "com.rently.keylessrn.Receiver"));
-                        context.sendBroadcast(successIntent);
+                        dpm.setLockTaskPackages(admin, new String[]{Constants.CLIENT_APP_PACKAGE});
+                        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(Constants.CLIENT_APP_PACKAGE);
+                        if (launchIntent != null) {
+                            context.startActivity(launchIntent);
+                        }
                     } else {
                         Log.i(Constants.TAG, "App is not an owner");
                     }
@@ -31,24 +66,11 @@ public class Receiver extends BroadcastReceiver {
                     Log.e(Constants.TAG, "Error in enableLockMode");
                     e.printStackTrace();
                 }
-                break;
             }
-
-            case Constants.ACTION_DISABLE_LOCK_MODE: {
-                try {
-                    DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-                    if (dpm.isDeviceOwnerApp(context.getPackageName())) {
-                        dpm.clearDeviceOwnerApp(context.getPackageName());
-                        ComponentName admin = new ComponentName(context.getApplicationContext(), SmartHomeDeviceAdminReceiver.class);
-                        dpm.removeActiveAdmin(admin);
-                        Log.i(Constants.TAG, "Removed as device owner");
-                    } else {
-                        Log.i(Constants.TAG, "App is not an owner");
-                    }
-                } catch (Exception e) {
-                    Log.e(Constants.TAG, "Error in disableLockMode");
-                    e.printStackTrace();
-                }
+        } else if (action.equals(Intent.ACTION_BOOT_COMPLETED) || action.equals(Intent.ACTION_LOCKED_BOOT_COMPLETED)) {
+            Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(Constants.CLIENT_APP_PACKAGE);
+            if (launchIntent != null) {
+                context.startActivity(launchIntent);
             }
         }
     }
